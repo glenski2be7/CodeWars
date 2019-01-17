ddcfbf8c78be20567f2458e67532158c74136fac
Łukasz Waręcki <glenski2be7@gmail.com>
2019-01-17 22:32:54 +0100

    Add properties to pom.xml

--- src/main/java/pl/infoshare/lukasz/ReturnNegative.java
+++ src/main/java/pl/infoshare/lukasz/ReturnNegative.java
@@ -3,7 +3,7 @@ package pl.infoshare.lukasz;
 public class ReturnNegative {
 
     public static int makeNegative(final int x) {
-        return Math.abs(x) * -1;
+        return Math.abs(x) * (-1);
         //x *= -1;
         //return x;
     }
