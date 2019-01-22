30f1263d82584739a79dc5b84cc28918f312b4b9
Łukasz Waręcki <glenski2be7@gmail.com>
2019-01-22 07:54:09 +0100

    SQL Basics: Simple JOIN

---
+++ src/main/java/pl/infoshare/lukasz/sql/SQL-BasicsSimpleJOIN.sql
@@ -0,0 +1,18 @@
+--For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
+
+--products table schema
+--id
+--name
+--isbn
+--company_id
+--price
+--companies table schema
+--id
+--name
+--You should return all product fields as well as the company name as "company_name".
+
+SELECT products.id, products.name, products.isbn,
+products.company_id,
+companies.name company_name
+FROM products
+inner JOIN companies ON products.company_id = companies.id
