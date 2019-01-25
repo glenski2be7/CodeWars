2134d6fa174e671ce693e4d3e9284d41bf69af23
Łukasz Waręcki <glenski2be7@gmail.com>
2019-01-25 13:12:11 +0100

    Add CalculateAverafgeFromArray, CalculateAverafgeFromArrayTest classes

--- src/main/java/pl/infoshare/lukasz/App.java
+++ src/main/java/pl/infoshare/lukasz/App.java
@@ -1,16 +1,16 @@
 package pl.infoshare.lukasz;
 
+import pl.infoshare.lukasz.sql.CalculateAverafgeFromArray;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println(ReturnNegative.makeNegative(-8));
         System.out.println(FindTheSmallestIntegerInTheArray.findSmallestInt(new int[]{3, 5, 1}));
         System.out.println(BasicVariableAssignment.name);
+        System.out.println("Srednia: " + CalculateAverafgeFromArray.find_average(new int[]{1, 2}));
     }
 }
